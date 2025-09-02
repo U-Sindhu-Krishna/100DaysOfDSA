3e2232eb26d24e108a40e62c52eb177d27331c9e
sindhu-krishna <17sindhukrishna@gmail.com>
2025-09-02 23:56:06 +0530

    Day 03: Check if Array is Sorted & Rotated

--- CheckSortedRotated.java
+++ CheckSortedRotated.java
@@ -1,16 +1,18 @@
 public class CheckSortedRotated {
     public static boolean isSortedAndRotated(int[] arr) {
         int n = arr.length;
-        int count = 0;
+        int dropCount = 0; // count how many times order breaks
 
         for (int i = 0; i < n; i++) {
+            // compare current with next (circular check using %)
             if (arr[i] > arr[(i + 1) % n]) {
-                count++;
+                dropCount++;
             }
-            if (count > 1)
-                return false;
+            if (dropCount > 1) {
+                return false; // if breaks more than once, not sorted+rotated
+            }
         }
-        return count == 1;
+        return dropCount == 1;
     }
 
     public static void main(String[] args) {
@@ -18,8 +20,8 @@ public class CheckSortedRotated {
         int[] arr2 = { 1, 2, 3, 4, 5 };
         int[] arr3 = { 3, 5, 4, 1, 2 };
 
-        System.out.println("Array 1 sorted & rotated? " + isSortedAndRotated(arr1));
-        System.out.println("Array 2 sorted & rotated? " + isSortedAndRotated(arr2));
-        System.out.println("Array 2 sorted & rotated? " + isSortedAndRotated(arr3));
+        System.out.println("Array 1 is sorted & rotated? " + isSortedAndRotated(arr1));
+        System.out.println("Array 2 is sorted & rotated? " + isSortedAndRotated(arr2));
+        System.out.println("Array 2 is sorted & rotated? " + isSortedAndRotated(arr3));
     }
 }
